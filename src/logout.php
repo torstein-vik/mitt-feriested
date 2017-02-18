d452d8e10af8c90571ad3d59c142c310168f8e0a
Torstein Vik <torsteinv64@gmail.com>
2017-02-18 23:09:00 +0100

    Fixed bug where log out only removed "user"-tag and not "userid" and "admin"

--- src/logout.php
+++ src/logout.php
@@ -1,6 +1,8 @@
 <?php
     if ($auth){
         unset($_SESSION["user"]);
+        unset($_SESSION["userid"]);
+        unset($_SESSION["admin"]);
         header("Refresh:0");
     } else {
         echo 'You\'re logged out!';
