f0002e8ba7b5ea1123bc029c3ac3c8886aecbb73
Torstein Vik <torsteinv64@gmail.com>
2017-02-18 23:47:04 +0100

    Added mypage redirect to login if not logged in

---
+++ src/mypage.php
@@ -0,0 +1,10 @@
+<?php
+    if(!$auth){
+        header("Location: ?page=login&redir=mypage");
+        return;
+    }
+    $name = $_SESSION["user"];
+    $userid = $_SESSION["userid"];
+    $admin = $_SESSION["admin"];
+?>
+
