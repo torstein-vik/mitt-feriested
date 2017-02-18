b6496033745d5d6a508b10d6cbed3599e43b84d0
Torstein Vik <torsteinv64@gmail.com>
2017-02-18 13:06:48 +0100

    Added javascript and some fonts

--- src/index.php
+++ src/index.php
@@ -4,7 +4,18 @@
 <html>
     <head>
         <title>Faroe Adventure</title>
+        <meta charset="utf-8" />
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans" />
+        <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
+
         <link rel="stylesheet" href="index.css"/>
+        <script src="index.js"></script>
+
+        <script>
+            $(init);
+        </script>
     </head>
     <body>
 
