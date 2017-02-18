6dac78546932a4df9c478af5a223d50c380cb8f4
Torstein Vik <torsteinv64@gmail.com>
2017-02-18 16:55:45 +0100

    Added sql script and database model

---
+++ create_database.sql
@@ -0,0 +1,46 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Database of mitt-feriested
+-- -----------------------------------------------------
+
+CREATE SCHEMA IF NOT EXISTS `mitt-feriested` DEFAULT CHARACTER SET utf8 ;
+USE `mitt-feriested` ;
+
+-- User table
+CREATE TABLE IF NOT EXISTS `mitt-feriested`.`users` (
+  `userid` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `passhash` BINARY(64) NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+ENGINE = InnoDB;
+
+-- Attaction table
+CREATE TABLE IF NOT EXISTS `mitt-feriested`.`attractions` (
+  `attractionid` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `pagefile` VARCHAR(45) NULL,
+  PRIMARY KEY (`attractionid`))
+ENGINE = InnoDB;
+
+-- Tip table
+CREATE TABLE IF NOT EXISTS `mitt-feriested`.`tips` (
+  `tipid` INT NOT NULL AUTO_INCREMENT,
+  `userid` INT NOT NULL,
+  `attractionid` INT NOT NULL,
+  `timestamp` TIMESTAMP NULL,
+  `content` TEXT NULL,
+  PRIMARY KEY (`tipid`),
+  FOREIGN KEY (`userid`)
+    REFERENCES `mitt-feriested`.`users` (`userid`),
+  FOREIGN KEY (`attractionid`)
+    REFERENCES `mitt-feriested`.`attractions` (`attractionid`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
