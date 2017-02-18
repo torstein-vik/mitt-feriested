4c31aece6d6d8331d42859a1f11860d4d32c1c2b
Torstein Vik <torsteinv64@gmail.com>
2017-02-18 22:00:15 +0100

    Fixed create_attractions error and auto-increment

--- create_attractions.sql
+++ create_attractions.sql
@@ -1,3 +1,7 @@
+-- If you get Error Code 1175, Go to Edit -> Preferences -> "SQL Editor" -> "Other" -> uncheck "Safe Updates". Then go to Query -> Reconnect to Server
+SET FOREIGN_KEY_CHECKS=0;
 DELETE FROM `mitt-feriested`.`attractions`;
+SET FOREIGN_KEY_CHECKS=1;
+ALTER TABLE attractions AUTO_INCREMENT = 1;
 INSERT INTO `mitt-feriested`.`attractions` (name, pagefile) VALUES ('test', 'test.php'),
 																   ('test2', 'test2.php');
