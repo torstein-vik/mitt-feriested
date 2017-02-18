2c6b79dbb57d44fc61deb1593c7d9ec5ea75c7dd
Torstein Vik <torsteinv64@gmail.com>
2017-02-18 21:45:24 +0100

    Added auto-deletion of attractions

--- create_attractions.sql
+++ create_attractions.sql
@@ -1,1 +1,3 @@
-INSERT INTO `mitt-feriested`.`attractions` (name, pagefile) VALUES ('test', 'test.php');
+DELETE FROM `mitt-feriested`.`attractions`;
+INSERT INTO `mitt-feriested`.`attractions` (name, pagefile) VALUES ('test', 'test.php'),
+																   ('test2', 'test2.php');
