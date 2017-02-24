a597dbb12aa26be6555d9e9715c5849dcfc08d52
Johannes Hansen Aas <Johannes Hansen Aas>
2017-02-24 18:13:14 +0100

    made a wrapper/body

--- src/home.php
+++ src/home.php
@@ -1,6 +1,8 @@
-<header>
-    <h1>The faroe islands!</h1>
-</header>
-<main>
-    <p>:(</p>
-</main>
+<div class="contentBody">
+    <header id="homeHeader">
+        <h1>The faroe islands!</h1>
+    </header>
+    <main class="mainContent">
+        <p>:(</p>
+    </main>
+</div>
