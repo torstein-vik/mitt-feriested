a5403814a872b5d3d28ba81d475b5ba960f83461
Johannes Hansen Aas <Johannes Hansen Aas>
2017-02-24 18:49:17 +0100

    slideshow

--- src/home.php
+++ src/home.php
@@ -1,3 +1,18 @@
+<div id="slideshow">
+    <div id="slide1">
+
+    </div>
+    <div id="slide2">
+
+    </div>
+    <div id="slide3">
+
+    </div>
+</div>
+
+
+
+
 <div class="contentBody">
     <header id="homeHeader">
         <h1>The faroe islands!</h1>
