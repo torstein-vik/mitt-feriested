2ec212cd746f84e9f50fa7a23971cd00b9f5798a
Johannes Hansen Aas <Johannes Hansen Aas>
2017-03-02 18:28:12 +0100

    some header decoration

--- src/addcomment.php
+++ src/addcomment.php
@@ -42,8 +42,14 @@
     } else {
         ?>
         <div class="contentBody">
-            <header>
+            <header class="contentHeader">
                 <h1> Add comment to <?php echo $attraction_name;?> </h1>
+                <svg id="grey-triangle1" width="190px" height="50px">
+                    <polygon points="0,50 190,0 190,50" style="fill:#A8A5A4;">
+                </svg>
+                <svg id="grey-triangle2" width="220px" height="65px">
+                    <polygon points="0,65 19,0 220,65" style="fill:#7C7972;">
+                </svg>
             </header>
             <main class="mainContent">
                 <form method="POST" action=<?php "?page=addcomment?a=".$_GET["a"]?>>
