5fab8cab7ffc6558e9e5f1e1076e3bb4ad1e1782
Johannes Hansen Aas <Johannes Hansen Aas>
2017-03-02 18:28:52 +0100

    stuff

--- src/register.php
+++ src/register.php
@@ -48,12 +48,25 @@
     } else {
         ?>
 
-        <form method="POST" action="?page=register">
-            <input name="username" type="text" placeholder="username">
-            <input name="password" type="password" placeholder="password">
-            <input name="conf_password" type="password" placeholder="confirm password">
-            <input type="submit" value="Register">
-        </form>
+        <div class="contentBody">
+            <header class="contentHeader">
+                <h1>Register for free today!</h1>
+                <svg id="grey-triangle1" width="190px" height="50px">
+                    <polygon points="0,50 190,0 190,50" style="fill:#A8A5A4;">
+                </svg>
+                <svg id="grey-triangle2" width="220px" height="65px">
+                    <polygon points="0,65 19,0 220,65" style="fill:#7C7972;">
+                </svg>
+            </header>
+            <main class="mainContent">
+                <form method="POST" action="?page=register">
+                    <input name="username" type="text" placeholder="username">
+                    <input name="password" type="password" placeholder="password">
+                    <input name="conf_password" type="password" placeholder="confirm password">
+                    <input type="submit" value="Register">
+                </form>
+            </main>
+        </div>
 
         <?php
     }
