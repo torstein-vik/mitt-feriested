7f13a95bcf1e7a11f383534d218547cecac53a41
Johannes Hansen Aas <Johannes Hansen Aas>
2017-03-07 21:06:28 +0100

    svg next and prev buttons

--- src/home.php
+++ src/home.php
@@ -9,8 +9,16 @@
         deg
     </div>
 
-    <a id="prev"> < </a>
-    <a id="next"> > </a>
+    <div id="prev">
+        <svg width="60px" height="160px">
+            <polygon points="45,30 15,80 45,130 30,80" style="fill:#333;" />
+        </svg>
+    </div>
+    <div id="next">
+        <svg width="60px" height="160px">
+            <polygon points="15,30 45,80 15,130 30,80" style="fill:#333" />
+        </svg>
+    </div>
 
 </div>
 
