aa75edcd63d7f6617e763853b2cbc3c9fd98c48e
Torstein Vik <torsteinv64@gmail.com>
2017-03-09 19:42:19 +0100

    Added data file, for retriving using ajax

---
+++ src/data.php
@@ -0,0 +1,13 @@
+<?php
+    if(isset($_GET["type"])){
+        $type = $_GET["type"];
+    } else {
+        return;
+    }
+
+    if($type == "attractions"){
+
+    } else {
+        return;
+    }
+?>
