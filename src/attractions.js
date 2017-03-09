b78444dbababbd433c2e2618c75ba02030cc3bfa
Torstein Vik <torsteinv64@gmail.com>
2017-03-09 20:18:19 +0100

    Added ajax code to attractions.js for getting attractions after filtering with tags.

--- src/attractions.js
+++ src/attractions.js
@@ -1,5 +1,28 @@
 function init(){
+    updateContent();
+
     $(".tagselector").click(function(){
         $(this).toggleClass("active");
     });
+}
+
+function updateContent(){
+    var flags = 0;
+
+    $("#attractions").html("Please wait...");
+
+    $(".tagselector.active").each(function(){
+        var tagid = $(this).attr('tagid');
+
+        flags |= (1 << (tagid - 1))
+    });
+
+
+    $.ajax({
+        url:("data?type=attractions?tags="+flags)
+    }).done(function(data){
+        setTimeout(function(){
+            $("#attractions").html(data);
+        }, 500);
+    })
 }
