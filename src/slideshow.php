9ae27c91276b658dc5affe7f4ab8d0866580b9a2
Torstein Vik <torsteinv64@gmail.com>
2017-03-10 14:50:10 +0100

    Moved slideshow to separate file

---
+++ src/slideshow.php
@@ -0,0 +1,23 @@
+<div id="slideShow">
+    <div id="slide1" class="slide">
+        hei
+    </div>
+    <div id="slide2" class="slide">
+        på
+    </div>
+    <div id="slide3" class="slide">
+        deg
+    </div>
+
+    <div id="prev">
+        <svg width="60px" height="160px">
+            <polygon points="45,30 15,80 45,130 30,80" style="fill:#333;" />
+        </svg>
+    </div>
+    <div id="next">
+        <svg width="60px" height="160px">
+            <polygon points="15,30 45,80 15,130 30,80" style="fill:#333" />
+        </svg>
+    </div>
+
+</div>
