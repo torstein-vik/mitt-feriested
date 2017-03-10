ebc11c1201241232a90be42013707e3abc87b982
Torstein Vik <torsteinv64@gmail.com>
2017-03-10 14:51:10 +0100

    Added contentBodies with headers to index php

--- src/index.php
+++ src/index.php
@@ -13,7 +13,8 @@
         "home" => [
             "page" => "home.php",
             "script" => "home.js",
-            "scriptinit" => "init"
+            "scriptinit" => "init",
+            "external" => "slideshow.php"
         ],
         "login" => [
             "page" => "login.php"
@@ -106,12 +107,31 @@
             </header>
             <main>
                 <?php
-                    if(isset($pages[$page]) ){
-                        include $pages[$page]["page"];
-                    } else {
-                        include $pages["404"]["page"];
+                    if(isset($pages[$page]) and isset($pages[$page]["external"]) ){
+                        include $pages[$page]["external"];
                     }
                 ?>
+
+                <div class="contentBody">
+                    <header id="homeHeader" class="contentHeader">
+                        <h1>The Faroe Islands!</h1>
+                        <svg id="homeHeaderSvg1" width="220px" height="55px">
+                            <polygon points="0,55 60,0 220,55" style="fill:#3f5e10;" />
+                        </svg>
+                        <svg id="homeHeaderSvg2" width="200px" height="125px">
+                            <polygon points="0,125 200,0 200,125" style="fill:#7C7972;" />
+                        </svg>
+                    </header>
+                    <main class="mainContent">
+                        <?php
+                            if(isset($pages[$page]) ){
+                                include $pages[$page]["page"];
+                            } else {
+                                include $pages["404"]["page"];
+                            }
+                        ?>
+                    </main>
+                </div>
             </main>
             <footer>
                 <div>&copy;Johannes Hansen Aas, Torstein Vik 2017</div>
