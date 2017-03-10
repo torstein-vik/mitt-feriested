69a5822b78bb6f27dde42201a5ab2d1af67c67e9
Torstein Vik <torsteinv64@gmail.com>
2017-03-10 16:06:21 +0100

    Added comment overview and deletions system to mypage

--- src/mypage.php
+++ src/mypage.php
@@ -7,5 +7,30 @@
     $userid = $_SESSION["userid"];
     $admin = $_SESSION["admin"];
 ?>
+<br>
+<h1>Your comments:</h1>
+<br>
+<?php
+$comment_query = $conn->query("SELECT tips.tipid, attractions.name, UNIX_TIMESTAMP(tips.timestamp), tips.content, tips.title FROM `mitt-feriested`.`attractions`, `mitt-feriested`.`tips` WHERE tips.userid=".$userid." ORDER BY tips.timestamp DESC;");
 
-<p>My comments:</p>
+        if(!$comment_query){
+            echo $conn->error;
+            return;
+        }
+
+        $comments = [];
+        while($row = $comment_query->fetch_assoc()){
+            $comments[] = $row;
+        }
+
+        foreach($comments as $comment){
+            ?>
+                <br><h3> <?php echo($comment['title']." - ".$comment["name"].' (posted '.date('d/m/Y', $comment["UNIX_TIMESTAMP(tips.timestamp)"]).")"); ?></h3>
+                <p>
+                    <?php echo($comment["content"]);?>
+                </p>
+                <p><a href="/api?type=deletecomment&tipid=<?php echo $comment["tipid"];?>">Click here to delete</a></p>
+
+            <?php
+        }
+?>
