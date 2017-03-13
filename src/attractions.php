f7713ab2613f04e7f5576f09e161c1e4fccc1b9b
Johannes Hansen Aas <Johannes Hansen Aas>
2017-03-13 23:43:07 +0100

    lkjvdj

--- src/attractions.php
+++ src/attractions.php
@@ -17,13 +17,10 @@
         include($attraction["pagefile"]);
 
 
-        echo "<h1> Weather near ".$attraction["name"].": </h1>";
+        echo "<h2> Weather near ".$attraction["name"].": </h2>";
 
         echo '<script src="http://www.yr.no/sted/'.$attraction["weather"].'/ekstern_boks_tre_dager.js"></script>';
         echo '<noscript><a href="http://www.yr.no/sted/'.$attraction["weather"].'/">Klikk her for å se værvarsel.</a></noscript>';
-
-
-        echo "<h1> Comments about ".$attraction["name"].": </h1>";
 
         $comment_query = $conn->query("SELECT users.username, users.privilege, UNIX_TIMESTAMP(tips.timestamp), tips.content, tips.title FROM `mitt-feriested`.`users`, `mitt-feriested`.`tips` WHERE users.userid=tips.userid AND tips.attractionid=".$attraction["attractionid"]." ORDER BY tips.timestamp DESC;");
 
@@ -37,31 +34,31 @@
             $comments[] = $row;
         }
 
-        foreach($comments as $comment){
-            ?>
-                <div class="comment">
-                    <h3> <?php echo($comment['title']); ?></h3>
-                    <h4><?php echo($comment["username"].($comment['privilege'] == 'admin' ? " [admin]" : "")." - ".date('d/m/Y', $comment["UNIX_TIMESTAMP(tips.timestamp)"])); ?></h4>
-                    <p>
-                        <?php echo($comment["content"]);?>
-                    </p>
-                </div>
-            <?php
-        }
+        echo '<div id="comments">';
+            echo "<h2> Comments about ".$attraction["name"].": </h2>";
+
+            foreach($comments as $comment){
+                ?>
+                    <div class="comment">
+                        <h3> <?php echo($comment['title']); ?></h3>
+                        <h4><?php echo($comment["username"].($comment['privilege'] == 'admin' ? " [admin]" : "")." - ".date('d/m/Y', $comment["UNIX_TIMESTAMP(tips.timestamp)"])); ?></h4>
+                        <p><?php echo($comment["content"]);?></p>
+                    </div>
+                <?php
+            }
+        echo '</div>';
 
         if($auth){
             ?>
-
                 <form method="POST" action="api?type=addcomment&a=<?php echo $_GET["a"]; ?>">
                     <h2>Leave a comment!</h2>
                     <input class="grey" name="title" type="text" placeholder="title">
                     <input name="comment" type="text" placeholder="comment">
                     <input type="submit" value="Add comment">
                 </form>
-
             <?php
         } else {
-            echo "<a href='?page=login'> Log in</a> or <a href='?page=register'> Register</a> to leave comments!";
+            echo "<a href='?page=login'>Log in</a> or <a href='?page=register'>Register</a> to leave comments!";
         }
 
     } else {
