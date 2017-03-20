541211f1bf465b50b2139b8d78fa5d168eb09ca8
Torstein Vik <torsteinv64@gmail.com>
2017-03-20 22:33:20 +0100

    Fixed indentation

--- src/login.php
+++ src/login.php
@@ -3,23 +3,23 @@
         header("Location: ?page=mypage");
     } else {
 
-            if (isset($_GET['redir'])){
-                $redir = $_GET['redir'];
-            } else {
-                $redir = 'mypage';
-            }
+        if (isset($_GET['redir'])){
+            $redir = $_GET['redir'];
+        } else {
+            $redir = 'mypage';
+        }
 
-            ?>
-            <form id="loginform" redir="<?php echo $redir;?>">
-                <input class="grey" style="border-radius: 5px 5px 0 0" name="username" type="text" placeholder="username">
-                <input name="password" type="password" placeholder="password">
-                <input class="grey" type="submit" value="Log in">
-                <p>Don't have a user? <a href="?page=register">Register here.</a></p>
-                <div id="errmsg"> </div>
-            </form>
+        ?>
+        <form id="loginform" redir="<?php echo $redir;?>">
+            <input class="grey" style="border-radius: 5px 5px 0 0" name="username" type="text" placeholder="username">
+            <input name="password" type="password" placeholder="password">
+            <input class="grey" type="submit" value="Log in">
+            <p>Don't have a user? <a href="?page=register">Register here.</a></p>
+            <div id="errmsg"> </div>
+        </form>
 
 
-            <?php
+        <?php
 
 
     }
