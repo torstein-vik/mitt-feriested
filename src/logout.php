7cd3b72462d06aae2868abde2c7d1c96237ff617
Torstein Vik <torsteinv64@gmail.com>
2017-03-20 22:37:26 +0100

    Forgot parenthesis in last commit

--- src/logout.php
+++ src/logout.php
@@ -5,7 +5,7 @@
         unset($_SESSION["admin"]);
         header("Refresh:0");
     } else {
-        if(isset($_GET["redir"]){
+        if(isset($_GET["redir"])){
             header("Location: ?page=".$_GET["redir"]);
         } else {
             header("Location: ?page=home");
