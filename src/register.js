f9c6790e635eab2a9986daade3fa6c2e8b3a9d5d
Torstein Vik <torsteinv64@gmail.com>
2017-03-20 23:05:14 +0100

    Made register use ajax as opposed to form w/ action to support dynamic error messages

--- src/register.js
+++ src/register.js
@@ -1,3 +1,25 @@
 function init(){
+    $("#registerform").submit(function(event){
+        event.preventDefault();
 
+        $("#errmsg").animate({width: "0"});
+
+        $.ajax({
+            method:"POST",
+            url:"api?type=register",
+            data:{
+                username: $("input[name='username']").val(),
+                password: $("input[name='password']").val(),
+                conf_password: $("input[name='conf_password']").val()
+            }
+        }).done(function (result){
+            if (result == "SUCCESS"){
+                location.href = "?page=" + $("#registerform").attr("redir");
+            } else {
+                $("#errmsg").html({PASSWD_MISMATCH_ERR: "The passwords don't match!", USERNAME_USED_ERR: "Username already in use!", UNKNOWN_ERR: "Unknown error. Please contact administator."}[result]);
+                $("#errmsg").animate({width: "500px"});
+
+            }
+        });
+    })
 }
