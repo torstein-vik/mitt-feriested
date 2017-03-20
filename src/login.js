80e6a513950fd4495b14a807e5445243d0239bc8
Torstein Vik <torsteinv64@gmail.com>
2017-03-20 22:24:49 +0100

    Migrated login code to api.php and implented ajax login, allowing dynamic error messages

---
+++ src/login.js
@@ -0,0 +1,24 @@
+function init(){
+    $("#loginform").submit(function(event){
+        event.preventDefault();
+
+        $("#errmsg").animate({width: "0"});
+
+        $.ajax({
+            method:"POST",
+            url:"api?type=login",
+            data:{
+                username: $("input[name='username']").val(),
+                password: $("input[name='password']").val()
+            }
+        }).done(function (result){
+            if (result == "SUCCESS"){
+                location.href = "?page=" + $("#loginform").attr("redir");
+            } else {
+                $("#errmsg").html({USERNAME_ERR: "No such username!", PASSWORD_ERR: "Wrong password!"}[result]);
+                $("#errmsg").animate({width: "500px"});
+
+            }
+        });
+    })
+}
--- src/login.php
+++ src/login.php
@@ -3,58 +3,24 @@
         header("Location: ?page=mypage");
     } else {
 
-        if(isset($_POST["username"]) and isset($_POST["password"])){
-            $username = $conn->real_escape_string($_POST["username"]);
-            $password = $conn->real_escape_string($_POST["password"]);
-
-            $username_find = $conn->query("SELECT * FROM `mitt-feriested`.`users` WHERE username = '".$username."'");
-
-            if($username_find->num_rows == 0){
-                ?>Wrong username<?php
-                return;
-            }
-
-            $user = $username_find->fetch_assoc();
-
-            $salt = bin2hex($user['passsalt']);
-
-            $hash = hash('sha256', $password.$salt);
-
-            if($hash == bin2hex($user["passhash"])){
-                $_SESSION['userid'] = $user["userid"];
-                $_SESSION['user'] = $username;
-                $_SESSION['admin'] = ($user['privilege'] == 'admin');
-
-
-                if (isset($_GET['redir'])){
-                    $redir = $_GET['redir'];
-                } else {
-                    $redir = 'login';
-                }
-
-                header('Location: ?page='.$redir);
-            } else {
-                echo "Wrong password!";
-                return;
-            }
-
-        } else {
             if (isset($_GET['redir'])){
                 $redir = $_GET['redir'];
             } else {
-                $redir = 'login';
+                $redir = 'mypage';
             }
 
             ?>
-            <form method="POST" action=<?php echo "?page=login&redir=".$redir;?>>
+            <form id="loginform" redir="<?php echo $redir;?>">
                 <input class="grey" style="border-radius: 5px 5px 0 0" name="username" type="text" placeholder="username">
                 <input name="password" type="password" placeholder="password">
                 <input class="grey" type="submit" value="Log in">
                 <p>Don't have a user? <a href="?page=register">Register here.</a></p>
+                <div id="errmsg"> </div>
             </form>
 
+
             <?php
-        }
+
 
     }
 ?>
