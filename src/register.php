d30626e6e9ef15dbb013340a2319b176f3010209
Torstein Vik <torsteinv64@gmail.com>
2017-03-20 22:42:17 +0100

    Migrated register code to api. Added error message support to html. Made other coding aspects match with login.php, to prepare for ajax registering.

--- src/register.php
+++ src/register.php
@@ -1,52 +1,24 @@
 <?php
-    if(isset($_POST["username"]) and isset($_POST["password"]) and isset($_POST["conf_password"])){
-        $username = $conn->real_escape_string($_POST["username"]);
-        $password = $conn->real_escape_string($_POST["password"]);
-        $password_conf = $conn->real_escape_string($_POST["conf_password"]);
-
-        if($password != $password_conf){
-            echo 'password mismatch!';
-            return;
-        }
-
-        $username_conflict = $conn->query("SELECT * FROM `mitt-feriested`.`users` WHERE username = '".$username."'");
-
-        if($username_conflict->num_rows > 0){
-            echo 'Username already in use!';
-            return;
-        }
-
-        print $conn->error;
-
-        $salt = bin2hex(random_bytes(32));
-
-        $hash = hash('sha256', $password.$salt);
-
-        $privilege = "'user'";
-
-        $query = $conn->query("INSERT INTO `mitt-feriested`.`users`  (username, passhash, passsalt, privilege) VALUES ('".$username."',0x".$hash.",0x".$salt.",".$privilege.");");
-
-        if($query){
-            echo "success! ";
-            include("login.php");
-
-        } else {
-            echo "failure!";
-            print $conn->error;
-        }
-
+    if ($auth){
+        header("Location: ?page=logout&redir=register");
     } else {
+
+        if (isset($_GET['redir'])){
+            $redir = $_GET['redir'];
+        } else {
+            $redir = 'mypage';
+        }
+
         ?>
-        <form method="POST" action="?page=register">
+        <form id="registerform" redir="<?php echo $redir;?>">
             <input class="grey" style="border-radius: 5px 5px 0 0" name="username" type="text" placeholder="username">
             <input  name="password" type="password" placeholder="password">
             <input class="grey" name="conf_password" type="password" placeholder="confirm password">
             <input type="submit" value="Register">
 
             <p>Already have a user? <a href="?page=login">Login here.</a></p>
+            <div id="errmsg"> </div>
         </form>
-
-
 
         <?php
     }
