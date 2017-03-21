fc72d5cfaa35761eb3f6cefc3f79c80def8350ee
torstein-vik <torsteinv64@gmail.com>
2017-03-21 11:14:56 +0100

    Added images to slideshow, updated some images, added some content to home

---
+++ src/home.php
@@ -0,0 +1,5 @@
+<div style="text-align:center; max-width: 600px; margin: 0 auto">
+<h2>Come to the faroe islands today!</h2>
+<br>
+This beatiful archipelagio is home to quite a few guys, and some sheep as well. Have an adventure with them! It's fun! The sheep are warm and fuzzy! You can also do some hiking on the beatiful mountains, visit some museums, or do some diving in the water!
+</div>
