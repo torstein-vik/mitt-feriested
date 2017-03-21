f192f35031bb9d0e46c0ac8f58380c8931cde24a
torstein-vik <torsteinv64@gmail.com>
2017-03-21 12:09:51 +0100

    Added content to barbara

---
+++ src/attr/barbara.php
@@ -0,0 +1,9 @@
+<h1> BARBARA FISH HOUSE </h1>
+Barbara Fish House, located in a cute house cut into the rocks, has a fantastic tasting menu, consisting of locally-caught Faroese seafood such as horse mussels and smoked salmon, fresh scallops and Bouillabaise poured from a teapot. If you want to try Faroese seafood, Barbara is the place to go.
+<br><br>
+<h4> Address: </h4>
+Gongin 4-6, 100 Tórshavn
+<br>
+Streymoy
+
+<br><br>
