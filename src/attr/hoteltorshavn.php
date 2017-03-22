f552101e7ca181bfc02323c7000ff11f7b4170b2
torstein-vik <torsteinv64@gmail.com>
2017-03-22 12:54:01 +0100

    Added hotel torshavn page

--- src/attr/hoteltorshavn.php
+++ src/attr/hoteltorshavn.php
@@ -1,1 +1,20 @@
-Hotel Tórshavn!
+<h1>HOTEL TÓRSHAVN</h1>
+This 3-star hotel is perfectly located in the centre of Tórshavn with a stunning view of the harbour and the Town Hall Square. Only a few mintues walk to the city centre. It includes a restaurant, café, cocktail bar and 6 excellent meeting rooms, suited for both large and small groups.
+<br><br>
+Whether you are 10, 30 or 60 people, the hotel can provide all the conference equipment needed. The conference rooms are equipped with projector, overhead, whiteboard, flip over, free wireless network and broadband.
+<br><br>
+There are 43 rooms on the hotel with a 73 bed capacity. The restaurant “Hvonn Brasserie” is on the first floor, and on the ground floor is a café & bar.
+<br><br>
+<h3>QUICK FACTS</h3>
+<ul>
+<li>Distance to Airport: 50 km</li>
+<li>Distance to City Centre: 0 km</li>
+<li>Hotel classification: 3 star</li>
+<li>Conference classification: 3 star</li>
+<li>Room capacity: 43</li>
+<li>Bed capacity: 73</li>
+<li>Restaurants: 1 (+ 1 café)</li>
+<li>Opened: 1923, renovated in 2007</li>
+<li>Services for people with physical handicap: Yes</li>
+</ul>
+<br><br>
