b4e7be34c202fe6253ab903a87bd0b18dc89a53c
torstein-vik <torsteinv64@gmail.com>
2017-03-22 12:59:59 +0100

    Added hotel vagar page

---
+++ src/attr/vagar.php
@@ -0,0 +1,16 @@
+<h1>HOTEL VÁGAR</h1>
+Hotel Vágar is a business- and tourist hotel which has 22 double rooms, 2 suites, and 3 meeting rooms, all fully equipped with AV, restaurant and bar. The 62ºN airport hotel – earlier named Hotel Vágar – is only 2 minutes’ walk from Vágar Airport.
+<br><br>
+Enjoy the silence at the hotel or a fresh walk to the village of Sørvágur, only a kilometre down the road. There you can take in the views of the the harbour and sea, or even take a boat trip to the fantastic island of Mykines.
+<br><br>
+<h3>QUICK FACTS</h3>
+<ul>
+<li>Distance to Airport: 0 km</li>
+<li>Distance to City Centre: 50 km</li>
+<li>Hotel classification: 3 star</li>
+<li>Room capacity: 24</li>
+<li>Bed capacity: 62</li>
+<li>Restaurants: 1</li>
+<li>Services for people with physical handicap: Yes</li>
+</ul>
+<br><br>
