8718ccd82b60b9d5e32190bc664e2d323312f894
torstein-vik <torsteinv64@gmail.com>
2017-03-22 12:03:26 +0100

    Added boat page

---
+++ src/attr/boat.php
@@ -0,0 +1,8 @@
+<h1>TRAVEL BY BOAT</h1>
+Smyril Line’s ferry, M/S Norröna, sails to the Faroe Islands from Hirtshals in the north of Denmark and from Seyðisfjørður in Iceland. Frequency of departures varies according to season.
+<br><br>
+<h2>SMYRIL LINE</h2>
+Start your journey on board the ferry, M/S Norröna – you’ll feel you’re on holiday as soon as you have the water beneath you. Travel in comfort, bring your own vehicle and the best part: no queuing!
+<br><br>
+Smyril Line offers transportation only as well as complete travel packages with accommodation (e.g. hotels or holiday houses). Slow travel at its finest!
+<br><br>
