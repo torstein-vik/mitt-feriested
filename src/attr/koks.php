71f70008d4343bc33b77c1c487e7eb95d8833203
torstein-vik <torsteinv64@gmail.com>
2017-03-22 13:13:56 +0100

    Added koks page

---
+++ src/attr/koks.php
@@ -0,0 +1,19 @@
+<h1>KOKS</h1>
+Widely regarded as the best restaurant in the Faroe Islands, Michelin star restaurant KOKS offers the complete fine dining experience.
+<br><br>
+Focusing on innovative traditional Faroese food, KOKS gives its guests the chance to taste the Faroes and its seasons through local produce.
+<br><br>
+The young avant-gardes at KOKS use Faroese produce, both coarse and fine, ancient and modern, always keeping an eye on sustainability, leaving the hills that bring forth the good fruit to flourish. Rather than chasing the novel for its own sake, every effort is put into exploring the ancient practices – drying, fermenting, salting and smoking.
+<br><br>
+KOKS follows the seasons and what they bring forth, transforming ancient culinary tradition into modern delicacies.
+<br><br>
+Simple and pure, fresh and traditional.
+<br><br>
+Fun fact: In 2015, KOKS won the The Nordic Prize for best restaurant in the Nordic countries and in 2017 was awarded a Michelin star, the first for a Faroese restaurant.
+<br><br>
+
+<br><br>
+<h3>ADDRESS</h3>
+Í Geilini 13, 175 Kirkjubøur<br>
+Streymoy
+<br><br>
