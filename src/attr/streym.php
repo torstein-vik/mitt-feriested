515382036bc3018857fff9c817fd28d049033971
torstein-vik <torsteinv64@gmail.com>
2017-03-22 12:57:39 +0100

    Added streym hotel page

--- src/attr/streym.php
+++ src/attr/streym.php
@@ -1,1 +1,22 @@
-Hotel Streym Hotell!
+<h1>HOTEL STREYM</h1>
+Hotel Streym is located in the centre of Tórshavn, while still featuring a seaside view.
+<br><br>
+The hotel aims to offer the freshest hotel experience possible. Their “no frills philosophy” means: everything you need, without all that extra stuff that you never use but still end up paying for. Next to the breakfast restaurant you’ll find some comfy couches where you can relax in front of the flatscreen-tv, with a complementary cup of coffee.
+<br><br>
+However, “no frills” doesn’t mean no contact to the world. At Hotel Streym they know how important staying in touch is, whether it’s for work or personal use, the broadband internet connection is always at your disposal, for free.
+<br><br>
+<h3>QUICK FACTS</h3>
+<ul>
+<li>Distance Airport: 45 km</li>
+<li>Distance to City Centre: 0,5 km</li>
+<li>Hotel classification: 3 star</li>
+<li>Room capacity: 36</li>
+<li>Bed capacity: 48</li>
+<li>Conference capacity: 40</li>
+<li>Restaurants: 1 (breakfast only)</li>
+</ul>
+<br><br>
+<h3>ADDRESS</h3>
+Yviri við Strond 19, 100 Tórshavn
+Streymoy
+<br><br>
