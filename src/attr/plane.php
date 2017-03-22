af1d7296ece58629aff1b6c23ed794a572fd4f24
torstein-vik <torsteinv64@gmail.com>
2017-03-22 13:22:58 +0100

    Added plane page file

---
+++ src/attr/plane.php
@@ -0,0 +1,11 @@
+<h1>TRAVEL BY PLANE</h1>
+Two airlines fly to the Faroe Islands. These are the Faroe Islands’ national airline, Atlantic Airways, and Scandinavian Airlines (also called SAS).
+<br><br>
+<h2>ATLANTIC AIRWAYS</h2>
+Atlantic Airways offers direct flights from a host of destinations, such as Denmark, Scotland, Iceland and Norway, with flights usually lasting no longer than two hours. The main route is from Copenhagen, where there are at least two flights daily all year. Frequency of departures to all destinations varies according to season. In addition, Atlantic Airways has direct seasonal flights to other destinations, such as Barcelona and Mallorca.<br>
+<a href="https://www.atlantic.fo/en">Book here</a>
+<br><br>
+<h2>SCANDINAVIAN AIRLINES</h2>
+SAS will start flying directly from Copenhagen, Denmark, to the Faroe Islands and back on 26 March, 2017. One flight daily, all year round.<br>
+<a href="https://www.flysas.com/en/uk">Book here</a>
+<br><br>
