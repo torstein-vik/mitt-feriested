25db7777e21d8e8663f839c1611e9b5368f71ef5
torstein-vik <torsteinv64@gmail.com>
2017-03-22 12:41:29 +0100

    Added Hotel Føroyar page

--- src/attr/hotelfoeroyar.php
+++ src/attr/hotelfoeroyar.php
@@ -1,1 +1,15 @@
-Hotel Føroyar!
+<h1> HOTEL FØROYAR </h1>
+
+<img src="attr/hotelf%C3%B8royar.jpeg" style="float: right"/>
+
+Hotel Føroyar (formerly Hotel Borg) is a four-star hotel above the Faroese capital of Tórshavn. Next to the Hotel Hafnia, it is the only four-star hotel in the country as well as its largest hotel with 216 beds.
+<br><br>
+A hotel of the same name existed at least before 1968, when it was used for local conferences and meetings by the Viking Congress. The current hotel was established in May 1983 as Hotel Føroyar, later named Hotel Borg, when shipowner Jákup Joensen (also called Jákup í Lopra) bought the hotel. The distinct building, a flat construction with a traditional grass roof and 216 beds, was designed by architects Friis & Moltke from Denmark.
+<br><br>
+In 1991, the shipping company Smyril Line purchased the hotel and changed its name back to Hotel Føroyar. However the hotel was sold to a group of the hotel's staff in 2005, it was sold for the benefit of refinancing the modern ferry line Norröna. The hotel restaurant, Restaurant KOKS, run by chef Leif Sørensen, serves international and Faroese cuisine, and has been cited by the Nordic Council of Ministers as the "best restaurant on the Faroe Islands". The buffet of the restaurant regularly serves traditional Faroese meat dishes such as skerpikjøt (dried mutton), dried whale meat, dried fish and blubber. The hotel is also regularly used for local and international conferences and events conferences, particularly by organizations operating between the Nordic countries, and weddings. It also organizes horse riding trips into the countryside for its guests.
+<br><br>
+
+<h4>Address:</h4>
+Hotel Føroyar<br>
+Oyggjarvegur 45<br>
+FO-100 Tórshavn<br>
