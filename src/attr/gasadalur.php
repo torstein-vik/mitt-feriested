3edca4c436a007099f113da52cd8ac649ee018f3
torstein-vik <torsteinv64@gmail.com>
2017-03-22 12:30:12 +0100

    Added gásadalur page

---
+++ src/attr/gasadalur.php
@@ -0,0 +1,31 @@
+<h1>BØUR GÁSADALUR</h1>
+<h3>HIKE ALONG THE OLD VILLAGE PATH TO THE BEAUTIFUL VILLAGE OF GÁSADALUR, WHICH WAS ONE OF THE MOST ISOLATED VILLAGES IN THE FAROE ISLANDS UNTIL A FEW YEARS AGO.</h3>
+<br>
+The route starts in the valley about three kilometres west of the village of Bøur. The path begins about one kilometre south (or left) of the tunnel entrance. You can see the path, which meanders up the Gásadalsbrekkan.
+<br><br>
+Gásadalur has been one of the most isolated villages in the Faroes. It is difficult to get to the village by sea and it was not until 2004 that the village was connected to the rest of the island by road when the tunnel was built. The people of Gásadalur used to walk this path when they had to go to the neighbouring villages to trade or for other errands. The helicopter service began in 1983.
+<br><br>
+The first stretch of the path is steep and runs close to the edge of the mountain . Therefore, walk extremely carefully, but do not forget to enjoy the outstanding view over Sørvágsfjørður, Tindhólmur, Gáshólmur and Mykines. In 2014, two men managed to climb all fives peaks of Tindhólmur.
+<br><br>
+There is no church in Gásadalur so the school is used for services. The cemetery is from 1873. Before then, people were burried in Bøur and so the coffin had to be carried over the mountain to Bøur from Gásadalur. The trip was very difficult and the only place that the bearers could rest was at the Líksteinurin  (Corpse Stone), which you will come upon halfway through the route. Further on, you come to the spring Vígdá . There is a story that a baby in Gásadalur became seriously ill and had to be taken to the doctor in Bøur. On the way to Bøur, the baby’s condition worsened and it was about to die. According to the Lutheran faith, your soul does not gain salvation if you die unbaptised. Therefore, the priest, who was travelling with them, quickly blessed the spring and baptised the baby.
+<br><br>
+When you continue, you will see Risasporið . There is a legend about two giants. One lived in Gásadalur and the other in Mykines. Once, they quarrelled and the Gásadalur giant wanted to go to Mykines to settle the dispute. He took running leaps along the mountain, took off, and with one leap, he landed on Mykines. He took off so hard that you can see his footprint to this day.
+<br><br>
+From Skarði, the path twists down the mountainside to the village. Beware of loose stones! The view down to Gásadalur is one of the most beautiful sights you can experience. The small, beautiful village is surrounded by green infield with harsh high mountains. One of them is Árnafjall, which, with its 722 metres, is the tallest mountain on Vágar. In the village, there are ruins from the Middle Ages, called Uppi við Garð and Gæsutoftir.
+<br><br>
+<h2>FACTS</h2>
+<strong>Duration:</strong> Two and a half hours<br>
+
+<strong>Distance:</strong> Three and a half km<br>
+
+<strong>Difficulty:</strong> Moderate to difficult. It is steep and difficult to climb Gásadalsbrekkan, and the descent to Gásadalur is also steep <br>
+
+<strong>Maximum height:</strong> 434 m <br>
+
+<strong>Children:</strong> Not suitable for children under eight to ten years, as the route is steep with many loose stones <br>
+
+<strong>Surface:</strong> Grass path. Down towards Gásadalur, path with stones and small rocks <br>
+
+<strong>Special comments:</strong> Be extra careful on the way down towards Gásadalur, as there are many loose stones, especially if you travel in a group<br>
+
+<br><br>
