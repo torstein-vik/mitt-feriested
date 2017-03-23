5d34e4f757eec5028451014df9b4559431d74585
torstein-vik <torsteinv64@gmail.com>
2017-03-23 14:36:56 +0100

    Formatted ræst page

--- src/attr/raest.php
+++ src/attr/raest.php
@@ -1,16 +1,17 @@
 <h1>RÆST</h1>
 <p>
 In May 2016, the Faroe Islands saw the opening of what might be the world’s first “fermented” restaurant: Raest, entirely dedicated to traditional Faroese fermented foods.
-</p>
+</p><br>
 <p>
 “Raest” means fermented in Faroese. Unlike the wet fermenting process for yogurt and pickled herring, the Faroes’ salty, brisk air creates ideal conditions for air-drying meat and fish, a process done in hjallur, food-drying sheds scattered across the islands.
-</p>
+</p><br>
 <p>
 The restaurant itself is in Tórshavn, the capital, on the main island of Streymoy, in a creaky 400-year-old house that makes Ingmar Bergman sets look Baroque. The narrow dining room’s floors, walls and ceilings are built of salvaged driftwood. Communal tables seat 27 and are made from the reclaimed Douglas pine of an old schooner’s mast. A Delft stove once fueled by blubber oil sits in the corner.
-</p>
+</p><br>
 <p>
 The menu includes cold fermented lamb soup with turnips, fermented cod and fermented lamb intestines, fermented colon on sauerkraut, Rhubarb porridge with cream of burned rosemary, and – for dessert – waffle, jam and milk.
-</p>
+</p><br>
 <h3>ADDRESS</h3>
 Gongin 8, 100 Tórshavn<br>
 Tórshavn (capital)
+<br><br>
