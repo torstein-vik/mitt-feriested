2a954281c9bff62948e058b1562cbd3f5a1481c2
torstein-vik <torsteinv64@gmail.com>
2017-03-23 14:35:08 +0100

    Added slættaratindur page

---
+++ src/attr/slaettaratindur.php
@@ -0,0 +1,31 @@
+<h1>SLÆTTARATINDUR</h1>
+<p>
+Slættaratindur, translated as “flat summit”, is the highest mountain in the Faroe Islands, towering at 880 metres. On a clear day, all 18 islands of the Faroe Islands can be viewed from the top (some claim that Iceland’s Vatnajökull mountain can also be seen!). On June 21, the longest day of the year, it is tradition to climb Slættaratindur and watch the sun set and rise again.
+</p><br>
+<p>
+There are two points from which to start climbing to reach the summit; the first, from Gjógv, takes about four hours of hiking; the second, from Eiðisskarð, takes under an hour.
+</p><br>
+<p>
+As the country’s highest mountain, the mountain has an alluring effect on both old and young, Faroese and visitors. You start your trip at Eiðisskarð, which is the pass between Eiði and Funningur. Go over the fence at the parking lot where Eiðisskarð is highest. At this first leg, there is no path as such up to Slættaratindur, but when you go up, stay in a straight line from the parking lot. The fence is on your right-hand side. When you get to about 670 metres’ altitude, or after about a 30-40 minutes’ walk, you will come to the path that leads to the top . The path is inclined uphill to the left. Here, you are already so high that you have a view of, e.g. Haldórsvík, with the eight-sided church and a string of mountains that encircle the villages in the north. Throughout the hike, you will be accompanied by sheep that graze at the top of the Faroe Islands.
+</p><br>
+<p>
+Follow the path one kilometre uphill. You then come to a point about 30 vertical metres from the top where the path turns a bend and goes to the right again . There are several trails of the path up here, but it is recommended that you ascend up on the back of the mountain where you turn the corner to the right. After a few metres, you are on the path again where you proceed uphill, keeping the top to your right. Be careful here, as it is gravelly and there may be loose stones. The last few metres up to the top are like a staircase made of rocks. Here, you also need to be careful because it is steep on your left-hand side. Feel that sense of butterflies in your stomach when you’ve reached the top. The view is nothing less than spectacular; especially, if you are lucky with the weather. On a clear, sunny day, the view covers practically all of the Faroe Islands. It is often said that if the weather is very clear and dry, you can see right across to Vatnajøkull in Iceland. The distance is 550 kilometres. Experts are, however, in some doubt as to whether this is possible and point to the possibility of this being a mirage.
+</p><br>
+<p>
+There is a tradition in the Faroe Islands to climb Slættaratindur on 21 June, the longest day of the year. At the top of the mountain, people can experience the sunset and then, a few hours later, watch the sun rise again. Time spent waiting for the sun to rise is used for traditional chain dancing and singing, and packed lunches typically consist of skerpikjøt (wind-dried mutton), dried fish and whale meat, along with a strong cup of coffee to help with staying awake. Go down the same road as you walked up. Since it can be difficult to pinpoint where you came up on top, it can be a good idea to put a backpack, a colourful scarf or sweater by the “stairs” so you can find the spot to descent again. Be careful on the way down because the gravel can be slippery on a dry day, just as the rocks can be slippery in the wet.
+</p><br>
+<h3>TWO METRES THAT DISAPPEARED</h3>
+<p>
+Faroese schoolchildren will have to learn about the mountains all over again in geography lessons. New measurements of the highest mountain, Slættaratindur, show that the country’s highest peak is two metres lower than previously assumed.
+</p><br>
+<p>
+Old measurements said 882 metres above sea level, but the new say 880 metres above sea level. Slættaratindur is still comfortably in the lead as the country’s highest mountain. The second highest, Gráfelli, just opposite Slættaratindur, is 856 metres.
+</p><br>
+<p>
+<h2>FACTS</h2>
+<strong>Duration:</strong> Two to two and a half hours<br>
+<strong>Difficulty:</strong> Moderate. A little difficult from Eiðisskarð and up to the path that leads to the top<br>
+<strong>Distance:</strong> Five to six km<br>
+<strong>Maximum height:</strong> 880 m<br>
+<strong>Children:</strong> The tour is suitable for children. However, keep an eye on children at the top, which is very steep in places<br>
+<strong>Surface:</strong> Partly visible grass path at the initial part. Then rockier path and rocks<br>
