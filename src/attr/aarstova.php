f4bbf3825d70b71fe031aba41003180d3f7dce29
torstein-vik <torsteinv64@gmail.com>
2017-03-23 14:44:19 +0100

    Added áarstova page

---
+++ src/attr/aarstova.php
@@ -0,0 +1,9 @@
+<h1>ÁARSTOVA</h1>
+<p>
+Áarstova used to be a family home on the outskirts of the Tórshavn. Today, it provides the setting for a cozy wooden restaurant located in the heart of the city – undoubtedly one of the best restaurants in the Faroe Islands. The impeccable service at Áarstova is only matched by the food and wine menu, which includes fabulous Faroese lamb and carefully chosen wine and Scottish ales.
+</p>
+<br>
+<h3>ADDRESS</h3>
+Gongin 1, 100 Tórshavn<br>
+Streymoy
+<br><br>
