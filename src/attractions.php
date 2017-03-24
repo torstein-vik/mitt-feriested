7ed2635cd2224568345266058966a4adede344ce
Torstein Vik <torsteinv64@gmail.com>
2017-03-24 22:10:42 +0100

    Made comment into textarea

--- src/attractions.php
+++ src/attractions.php
@@ -60,7 +60,7 @@
                 <form method="POST" action="api?type=addcomment&a=<?php echo $_GET["a"]; ?>">
                     <h2>Leave a comment!</h2><br>
                     <input class="grey" style="border-radius: 5px 5px 0 0" name="title" type="text" placeholder="title">
-                    <input name="comment" type="text" placeholder="comment">
+                    <textarea name="comment" placeholder="comment"> </textarea>
                     <input type="submit" value="Add comment">
                 </form>
             <?php
