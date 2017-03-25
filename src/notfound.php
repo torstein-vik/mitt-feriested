a9fc28c625c565c2cbb7aa4e1c265f606847da96
Torstein Vik <torsteinv64@gmail.com>
2017-03-25 14:07:25 +0100

    Added custom 404-page, must be configuered in httpd!

--- src/notfound.php
+++ src/notfound.php
@@ -1,3 +1,11 @@
 <center>
-    Couldn't find page "<?php echo $_GET["page"]?>"
+    <?php
+        if(apache_getenv("REDIRECT_URL")){
+            echo 'Couldn\'t find page "'.apache_getenv("REDIRECT_URL").'"';
+        } else if (isset($_GET["page"])){
+            echo 'Couldn\'t find page "'.$_GET["page"].'"';
+        } else {
+            echo 'Couldn\'t find page';
+        }
+    ?>
 </center>
