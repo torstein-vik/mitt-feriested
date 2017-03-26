e866665349b21421269efb65f3a49b7fe6d8d071
Johannes Hansen Aas <Johannes Hansen Aas>
2017-03-27 01:30:56 +0200

    fghfgh

--- src/home.php
+++ src/home.php
@@ -1,27 +1,25 @@
-<div>
-    <h2 style="text-align: center;">Europe's best kept secret</h2>
-
-    <br><br>
-    Adrift in the whirling rhythmic North Atlantic Sea, a different world lingers. A place like no other on earth. An idyllic escape, peacefully set among lush green valleys, imposing basalt cliffs, grand treeless moorlands and waterfalls plunging directly into the wind whipped ocean. Unfathomable beauty, mysteriously tucked away like a figment of a child’s imagination.
-    <br><br>
-
-    The 18 jagged volcanic islands which make up the Faroe Islands resemble a handful of rocks scattered haphazardly in the deep-sea ocean. Home to mountains of myth, hobbit-like turf-roofed houses and grazing shaggy sheep, these islands make up the perfect playground for the senses. They inexplicably unearth a soothing sound of stillness and a touch of rawness, only bettered by the taste of the freshest of air and the sight of sprawling landscapes in any direction. And the smell. That smell of unhurried childhood.
-    <br><br>
-
-    Fickle and highly changeable weather adds to the charm (or puts forth a challenge, depending on how you see it). Hold out both palms and you might feel snow falling on one palm and sun shining on the other. Nevertheless, the multitude of opportunities across the islands downplay the importance of weather. There is always something to experience, regardless.
-    <br><br>
-
-    <img src="attr/diving.jpg" style="width:100%"/>
-    <br><br>
-
-    The Faroese people are shaped and moulded by the harsh elements that have surrounded them for generations; a wonderful blend of isolation, robustness and reserve, open arms, genuineness and warmth.
-    <br><br>
-
-    Few communities as small can boast of an equally vibrant art and music scene, while Faroese gastronomy has never been more highly thought of. The decidedly slow pace of life on the islands lends itself to hospitable kinfolk and laid-back living.
-    <br><br>
-
-    But all is not slow and easy-going. The islands are also an adrenaline junkie’s dream, with no shortage of fast-paced activities on land and sea.
-    <br><br>
-
-    All of this and more creates a unique and inimitable destination at the edge of the world. A place truly unspoiled, unexplored and unbelievable.
+<div id="about">
+    <h1>Europe's best kept secret</h1>
+    <img src="res/flag.png"/>
+    <p>
+        Adrift in the whirling rhythmic North Atlantic Sea, a different world lingers. A place like no other on earth. An idyllic escape, peacefully set among lush green valleys, imposing basalt cliffs, grand treeless moorlands and waterfalls plunging directly into the wind whipped ocean. Unfathomable beauty, mysteriously tucked away like a figment of a child’s imagination.
+    </p>
+    <p>
+        The 18 jagged volcanic islands which make up the Faroe Islands resemble a handful of rocks scattered haphazardly in the deep-sea ocean. Home to mountains of myth, hobbit-like turf-roofed houses and grazing shaggy sheep, these islands make up the perfect playground for the senses. They inexplicably unearth a soothing sound of stillness and a touch of rawness, only bettered by the taste of the freshest of air and the sight of sprawling landscapes in any direction. And the smell. That smell of unhurried childhood.
+    </p>
+    <p>
+        Fickle and highly changeable weather adds to the charm (or puts forth a challenge, depending on how you see it). Hold out both palms and you might feel snow falling on one palm and sun shining on the other. Nevertheless, the multitude of opportunities across the islands downplay the importance of weather. There is always something to experience, regardless.
+    </p>
+    <p>
+        The Faroese people are shaped and moulded by the harsh elements that have surrounded them for generations; a wonderful blend of isolation, robustness and reserve, open arms, genuineness and warmth.
+    </p>
+    <p>
+        Few communities as small can boast of an equally vibrant art and music scene, while Faroese gastronomy has never been more highly thought of. The decidedly slow pace of life on the islands lends itself to hospitable kinfolk and laid-back living.
+    </p>
+    <p>
+        But all is not slow and easy-going. The islands are also an adrenaline junkie’s dream, with no shortage of fast-paced activities on land and sea.
+    </p>
+    <p>
+        All of this and more creates a unique and inimitable destination at the edge of the world. A place truly unspoiled, unexplored and unbelievable.
+    </p>
 </div>
