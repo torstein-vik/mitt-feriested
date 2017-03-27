af8cd86bd6de3f9a8611008df48d32a248c889b4
Johannes Hansen Aas <Johannes Hansen Aas>
2017-03-27 02:12:47 +0200

    ksjdfh

--- src/home.php
+++ src/home.php
@@ -1,13 +1,18 @@
 <div id="about">
+    <div id="img1">
+        <img src="res/flag.png"/>
+    </div>
     <h1>Europe's best kept secret</h1>
-    <img src="res/flag.png"/>
     <p>
         Adrift in the whirling rhythmic North Atlantic Sea, a different world lingers. A place like no other on earth. An idyllic escape, peacefully set among lush green valleys, imposing basalt cliffs, grand treeless moorlands and waterfalls plunging directly into the wind whipped ocean. Unfathomable beauty, mysteriously tucked away like a figment of a child’s imagination.
     </p>
+    <div id="img2">
+        <img src="res/flag.png"/>
+    </div>
     <p>
         The 18 jagged volcanic islands which make up the Faroe Islands resemble a handful of rocks scattered haphazardly in the deep-sea ocean. Home to mountains of myth, hobbit-like turf-roofed houses and grazing shaggy sheep, these islands make up the perfect playground for the senses. They inexplicably unearth a soothing sound of stillness and a touch of rawness, only bettered by the taste of the freshest of air and the sight of sprawling landscapes in any direction. And the smell. That smell of unhurried childhood.
     </p>
-    <p>
+    <p style="clear: both;">
         Fickle and highly changeable weather adds to the charm (or puts forth a challenge, depending on how you see it). Hold out both palms and you might feel snow falling on one palm and sun shining on the other. Nevertheless, the multitude of opportunities across the islands downplay the importance of weather. There is always something to experience, regardless.
     </p>
     <p>
